007ab1228d416071f52b7f291b4343e37461394a
Abdul Rehman <abdulshahin60@gmail.com>
2025-07-14 18:19:28 +0500

    Fix: Remove any from global in MongoDB connection utility

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -10,15 +10,19 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local');
 }
 
+// Extend NodeJS.Global type
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
 if (process.env.NODE_ENV === "development") {
-  // In dev, use a global variable so it doesn't re-create on hot reload
-  if (!(global as any)._mongoClientPromise) {
+  if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    (global as any)._mongoClientPromise = client.connect();
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = (global as any)._mongoClientPromise;
+  clientPromise = global._mongoClientPromise!;
 } else {
-  // In production, always create new
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
